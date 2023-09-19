a1ec13dbca26ade2b1aed029d229da4cc6330e36
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-19 20:15:51 -0300

    Initial jest tests 4

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,17 +1,16 @@
-import { render, screen } from '@testing-library/react'
-import Main from '.'
+import { render, screen } from "@testing-library/react";
+import Main from ".";
 
-describe('<Main />', () => {
-  it('should render the heading', () => {
+describe("<Main />", () => {
+  it("should render the heading", () => {
     //renderiza o componente
-    const { container } = render(<Main></Main>)
+    const { container } = render(<Main></Main>);
     //busca o elemento e verifica a existência
     expect(
-      screen.getByRole('heading',
-        { name: /Vue avançado/i}
-      )).toBeInTheDocument()
+      screen.getByRole("heading", { name: /react avançado/i }),
+    ).toBeInTheDocument();
 
     //gerar snapshot
-    expect(container.firstChild).toMatchSnapshot()
-  })
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });

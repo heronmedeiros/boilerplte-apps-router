15cde41a8334fa0b078c4d10d96d644f89289e93
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-19 20:14:43 -0300

    Initial jest tests 2

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
   "*.js,jsx,tsx}": (filenames) => [
     `prettier --write ${filenames.join(" ")}`,
     `npm run lint --fix . ${filenames.join(" --file")}`,
-    `npm test --findRelatedTests ${filenames.join(" ")}`,
+    `npm test -- --findRelatedTests ${filenames.join(" ")}`,
   ],
 };
